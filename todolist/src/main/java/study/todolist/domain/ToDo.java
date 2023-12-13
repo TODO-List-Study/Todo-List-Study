034adf3888aa9af8537be025a3aa311c01dae285
Lee.t.c <lsh6451217@gmail.com>
2023-12-14 01:11:25 +0900

    refactor: 투두 엔티티 필요 어노테이션 설정

--- todolist/src/main/java/study/todolist/domain/ToDo.java
+++ todolist/src/main/java/study/todolist/domain/ToDo.java
@@ -1,5 +1,12 @@
 package study.todolist.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ToDo {
     private Long id;
 
