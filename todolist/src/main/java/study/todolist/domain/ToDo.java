dc709ccfb869d767ae7c83edb05d76bcff8b98e8
Lee.t.c <lsh6451217@gmail.com>
2023-12-14 19:42:09 +0900

    refactor: ToDo PK값 필드 제거

--- todolist/src/main/java/study/todolist/domain/ToDo.java
+++ todolist/src/main/java/study/todolist/domain/ToDo.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ToDo {
-    private Long id;
 
     private ToDoEssential toDoEssential;
 
