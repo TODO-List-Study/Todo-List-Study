2728b54fa2695d4cc2adad30ec7058a80a5a4ef4
jhnyuk <debussysuh@gmail.com>
2023-12-22 02:27:11 +0900

    Feat: 사용자가 가진 권한을 나타내는 GrantedAuthority 인터페이스 추가

--- todolist/src/main/java/study/todolist/domain/member/entity/Member.java
+++ todolist/src/main/java/study/todolist/domain/member/entity/Member.java
@@ -5,7 +5,13 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import study.todolist.base.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
@@ -15,4 +21,14 @@ public class Member extends BaseEntity {
     @Column(unique = true)
     private String email;
     private String password;
+    private String username;
+
+    public Collection<? extends GrantedAuthority> getGrantedAuthorities() {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        // 모든 멤버는 member 권한을 가진다.
+        grantedAuthorities.add(new SimpleGrantedAuthority("member"));
+
+        return grantedAuthorities;
+    }
 }
