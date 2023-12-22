df7abd60a73727d066e4a2d68a5383a713eb3475
Jinwoo <zvzv9808@gmail.com>
2023-12-22 14:26:49 +0900

    refactor: TodoRepository 수정

--- todolist/src/main/java/study/todolist/repository/TodoRepository.java
+++ todolist/src/main/java/study/todolist/repository/TodoRepository.java
@@ -1,43 +1,9 @@
 package study.todolist.repository;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.todolist.entity.TodoList;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+public interface TodoRepository extends JpaRepository<TodoList, Long> {
 
-@Repository
-public class TodoRepository implements MemoryDBRepository<TodoList> {
 
-    private List<TodoList> DB = new ArrayList<>();
-    Long id = 0L;
-
-    @Override
-    public TodoList save(TodoList entity) {
-        entity.updateId(++id);
-        DB.add(entity);
-
-        return entity;
-    }
-
-    @Override
-    public Optional<TodoList> findById(Long id) {
-
-        return DB.stream()
-                .filter(todo -> todo.getId().equals(id))
-                .findFirst();
-    }
-
-    @Override
-    public void delete(Long id) {
-
-        DB.removeIf(todo -> todo.getId().equals(id));
-    }
-
-    @Override
-    public List<TodoList> findAll() {
-
-        return DB;
-    }
 }
