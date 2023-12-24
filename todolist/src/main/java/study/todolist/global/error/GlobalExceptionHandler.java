975d82db606f74c49bd88b91d3ea531640779056
Jinwoo <zvzv9808@gmail.com>
2023-12-25 01:02:35 +0900

    refactor: Todo 등록 최대 수 초과 예외 처리 handler 추가

--- todolist/src/main/java/study/todolist/global/error/GlobalExceptionHandler.java
+++ todolist/src/main/java/study/todolist/global/error/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import study.todolist.global.Envelope;
 import study.todolist.global.error.exception.BusinessException;
 import study.todolist.global.error.exception.EntityNotFoundException;
+import study.todolist.global.error.exception.OverMaxCountException;
 
 @Slf4j
 @RestControllerAdvice
@@ -19,6 +20,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public Envelope handleEntityNotFoundException(EntityNotFoundException e){
         log.error("EntityNotFoundException", e);
+
+        return Envelope.fail(e.getErrorCode().getHttpStatus().value(), e.getMessage());
+    }
+
+    @ExceptionHandler(OverMaxCountException.class)
+    public Envelope handleOverMaxCountException(OverMaxCountException e){
+        log.error("OverMaxCountException", e);
 
         return Envelope.fail(e.getErrorCode().getHttpStatus().value(), e.getMessage());
     }
