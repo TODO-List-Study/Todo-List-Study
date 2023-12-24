7d6ae7421cdfc7df21cedb477b6eb24431f4c396
jhnyuk <debussysuh@gmail.com>
2023-12-24 23:34:37 +0900

    Chore: 스케쥴링 어노테이션 추가

--- todolist/src/main/java/study/todolist/TodolistApplication.java
+++ todolist/src/main/java/study/todolist/TodolistApplication.java
@@ -2,8 +2,10 @@ package study.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TodolistApplication {
 
 	public static void main(String[] args) {
