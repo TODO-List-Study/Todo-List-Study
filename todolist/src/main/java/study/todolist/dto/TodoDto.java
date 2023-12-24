a13e4ad6260e385bc181d8cf446c8889681ffc00
Jinwoo <zvzv9808@gmail.com>
2023-12-25 00:16:39 +0900

    refactor: Response 객체 status 컬럼 수정

--- todolist/src/main/java/study/todolist/dto/TodoDto.java
+++ todolist/src/main/java/study/todolist/dto/TodoDto.java
@@ -4,7 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import study.todolist.entity.TodoList;
+import study.todolist.entity.todo.Status;
+import study.todolist.entity.todo.TodoList;
 
 public class TodoDto {
 
@@ -18,14 +19,14 @@ public class TodoDto {
     public static class Response{
         private Long id;
         private String title;
-        private boolean check;
+        private Status status;
 
         public static Response of(TodoList todoList) {
 
             return Response.builder()
                     .id(todoList.getId())
                     .title(todoList.getTitle())
-                    .check(todoList.isChecked())
+                    .status(todoList.getStatus())
                     .build();
         }
     }
