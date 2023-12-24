799b76f90426c557eb9fee532fe737951a9f60c7
jhnyuk <debussysuh@gmail.com>
2023-12-25 00:28:32 +0900

    Feat: Member Controller 추가

---
+++ todolist/src/main/java/study/todolist/domain/member/controller/MemberController.java
@@ -0,0 +1,28 @@
+package study.todolist.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import study.todolist.base.RsData;
+import study.todolist.domain.member.dto.MemberDto;
+import study.todolist.domain.member.entity.Member;
+import study.todolist.domain.member.service.MemberService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public RsData<Member> createMember(@RequestBody MemberDto memberDto) {
+        Member member = memberService.createMember(memberDto);
+        return RsData.successOf(member);
+    }
+
+    @GetMapping("/{id}")
+    public RsData<Member> getMember(@PathVariable Long id) {
+        Member member = memberService.findById(id);
+        return RsData.successOf(member);
+    }
+}
