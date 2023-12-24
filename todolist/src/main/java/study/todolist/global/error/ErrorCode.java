2aa6510e569ee46e0fe301fb4e2f7245d4bb20a0
Jinwoo <zvzv9808@gmail.com>
2023-12-25 00:56:14 +0900

    refactor: ErrorCode 정의

--- todolist/src/main/java/study/todolist/global/error/ErrorCode.java
+++ todolist/src/main/java/study/todolist/global/error/ErrorCode.java
@@ -6,7 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    NOT_FOUND_TODO(HttpStatus.NOT_FOUND, "T-001", "TODO를 찾을 수 없습니다.")
+    NOT_FOUND_TODO(HttpStatus.NOT_FOUND, "T-001", "TODO를 찾을 수 없습니다."),
+    OVER_MAX_COUNT(HttpStatus.BAD_REQUEST, "T-002", "한번에 생성 가능한 수를 초과하였습니다."),
+
+    NOT_FOUNT_MEMBER(HttpStatus.NOT_FOUND, "M-001", "Member를 찾을 수 없습니다.")
     ;
 
 
