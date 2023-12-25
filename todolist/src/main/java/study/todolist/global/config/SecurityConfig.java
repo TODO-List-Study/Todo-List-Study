d42a3da11a7bb754bf6879e6eaff5da47e890c8c
Lee.t.c <lsh6451217@gmail.com>
2023-12-26 00:25:46 +0900

    feat: Spring Security 인가처리 작성

---
+++ todolist/src/main/java/study/todolist/global/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package study.todolist.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http
+
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.POST, "/members/signup").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
+        // @formatter:on
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder customPasswordEncoder() {
+        return PasswordEncoderConfig.passwordEncoder();
+    }
+}
