6fe0dac36ed0ef6bcb4f6366bd61924366a871cc
Jinwoo <zvzv9808@gmail.com>
2024-01-04 22:23:06 +0900

    refactor: 회원 등록 관련 ErrorCode 추가

--- todolist/src/main/java/study/todolist/global/error/ErrorCode.java
+++ todolist/src/main/java/study/todolist/global/error/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
     NOT_FOUND_TODO(HttpStatus.NOT_FOUND, "T-001", "TODO를 찾을 수 없습니다."),
     OVER_MAX_COUNT(HttpStatus.BAD_REQUEST, "T-002", "한번에 생성 가능한 수를 초과하였습니다."),
 
-    NOT_FOUNT_MEMBER(HttpStatus.NOT_FOUND, "M-001", "Member를 찾을 수 없습니다.")
+    NOT_FOUNT_MEMBER(HttpStatus.NOT_FOUND, "M-001", "Member를 찾을 수 없습니다."),
+    ALREADY_REGISTERED_MEMBER(HttpStatus.BAD_REQUEST, "M-002", "이미 가입되어있는 Email입니다."),
     ;
 
 
