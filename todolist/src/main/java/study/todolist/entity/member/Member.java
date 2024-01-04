f51c15399aa78f747bb7f4afcefb122b413dc55b
Jinwoo <zvzv9808@gmail.com>
2024-01-04 19:59:33 +0900

    refactor: 회원 관리를 위한 컬럼 추가

--- todolist/src/main/java/study/todolist/entity/member/Member.java
+++ todolist/src/main/java/study/todolist/entity/member/Member.java
@@ -2,13 +2,14 @@ package study.todolist.entity.member;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import study.todolist.entity.todo.TodoList;
 
 import java.util.List;
 
-@Entity @Getter
+@Entity @Getter @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class Member {
 
@@ -17,6 +18,10 @@ public class Member {
     private Long id;
 
     private String username;
+
+    private String email;
+
+    private String password;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "member")
     private List<TodoList> todoList;
