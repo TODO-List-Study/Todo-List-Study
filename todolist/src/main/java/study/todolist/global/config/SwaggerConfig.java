dca81078f54fffe45259cba540713355f66fcbfc
jhnyuk <debussysuh@gmail.com>
2024-01-13 13:50:48 +0900

    Feat: RestAPI Docs 추가

---
+++ todolist/src/main/java/study/todolist/global/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package study.todolist.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Todo RestAPI Document")
+                .version("v0.1")
+                .description("Todo RestAPI Document");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
